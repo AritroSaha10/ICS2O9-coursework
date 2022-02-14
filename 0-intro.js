f3ab9dd85983edcbd1ab9ded0441b75889b56c4c
Aritro Saha <29025984+AritroSaha10@users.noreply.github.com>
2022-02-14 13:44:08 -0500

    Create 0-intro.js

---
+++ 0-intro.js
@@ -0,0 +1,18 @@
+/*
+   Description: Trying out p5.js
+   Author: Aritro Saha
+   Date of last edit: Feb7/22
+*/
+
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(255, 255, 255);
+}
+
+function draw() {
+	fill(200,50,220);
+	rect(50, 150, 125, 25);
+  // For fill, the values are likely RGB (Red, Green, Blue) values to represent the color
+  // For rect, the first two values represent the x and y values of the top left vertex of the rectangle, while the last two values represent the width and height of the rectangle (respectively)
+}
